34d96d04c65014d3df470f62eee55febf2f24b0b
Glad Chinda <gladxeqs@gmail.com>
2018-06-26 12:01:38 +0100

    Bug fix: Logs search route throws error when log file not found.

--- app/routes/logs.js
+++ app/routes/logs.js
@@ -28,13 +28,13 @@ router.get('/:type/search/:date', (req, res) => {
 	// Convert date param to moment
 	const dateMoment = moment(date, ['YYYYMMDD', 'DDMMYYYY']);
 
-	const allowedTypes = ['error', 'info'];
+	const allowedTypes = ['error', 'all'];
 	const timeRegex = /^(?:[0-1]\d|2[0-3])[0-5]\d$/;
 
 	// Check and resolve time range query params
 	const toTime = timeRegex.test(to) ? +to : null;
 	const fromTime = timeRegex.test(from) ? +from : null;
-	const validTimeRange = fromTime && toTime && fromTime <= toTime;
+	const validTimeRange = !(_.isNull(fromTime) || _.isNull(toTime)) && fromTime <= toTime;
 
 	// Handle invalid log type error response
 	if (!_.includes(allowedTypes, type.toLowerCase())) {
@@ -73,55 +73,59 @@ router.get('/:type/search/:date', (req, res) => {
 	// Regex for extracting log details from log line
 	const EXTRACT_REGEX = /^(?:[\s]*?)(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})\s([a-z]+):\s(.+)\r\n([\s\S]+?)$/i;
 
-	// Open and read the contents of the log file
-	const file = getFilePointer(path.resolve(ROOT_DIR, LOG_FILE))('r');
-	const content = fs.readFileSync(file).toString('utf-8');
+	try {
+    // Open and read the contents of the log file
+    const file = getFilePointer(path.resolve(ROOT_DIR, LOG_FILE))('r');
+    const content = fs.readFileSync(file).toString('utf-8');
 
-	// Extract the log lines from the log file
-	let logs = content.match(LOG_REGEX);
+    // Extract the log lines from the log file
+    let logs = content.match(LOG_REGEX);
 
-	// Handle empty log file response
-	if (!logs) {
+    // Handle empty log file response
+    if (!logs) {
+      return res.json({status: 'success', logs: []});
+    }
+
+    // Extract hrs and mins from the given time ranges
+    const [toHr, toMin] = moment(to, 'HHmm').format('HH:mm').split(':');
+    const [fromHr, fromMin] = moment(from, 'HHmm').format('HH:mm').split(':');
+
+    // Get the exact from timestamp and to timestamp
+    const toMoment = dateMoment.clone().set({h: toHr, m: toMin}).unix();
+    const fromMoment = dateMoment.clone().set({h: fromHr, m: fromMin}).unix();
+
+    logs = logs.map(log => {
+
+      // Extract log details from log line
+      const extracts = log.match(EXTRACT_REGEX);
+
+      // Handle empty extracted data
+      if (!extracts) return {log: log.trim()};
+
+      const [, datetime, level, message, meta = ''] = extracts;
+
+      // Return extracted data with proper parsing
+      return {
+        datetime, level, message,
+        meta: JSON.parse(meta.replace(/([\s]*?)(?=[\{\}\"])/g, ''))
+      };
+
+    }).filter(({datetime = null}) => {
+
+      // Resolve timestamp from log datetime
+      const dateMoment = moment(datetime);
+      const timestamp = dateMoment.unix();
+
+      // Verify that log falls within the required timestamp range
+      return datetime && dateMoment.isValid() && timestamp >= fromMoment && timestamp <= toMoment;
+
+    });
+
+    // Return the final logs success response
+    return res.json({status: 'success', logs});
+  } catch (e) {
 		return res.json({ status: 'success', logs: [] });
 	}
-
-	// Extract hrs and mins from the given time ranges
-	const [ toHr, toMin ] = moment(to, 'HHmm').format('HH:mm').split(':');
-	const [ fromHr, fromMin ] = moment(from, 'HHmm').format('HH:mm').split(':');
-
-	// Get the exact from timestamp and to timestamp
-	const toMoment = dateMoment.clone().set({ h: toHr, m: toMin }).unix();
-	const fromMoment = dateMoment.clone().set({ h: fromHr, m: fromMin }).unix();
-
-	logs = logs.map(log => {
-
-		// Extract log details from log line
-		const extracts = log.match(EXTRACT_REGEX);
-
-		// Handle empty extracted data
-		if (!extracts) return { log: log.trim() };
-
-		const [, datetime, level, message, meta = ''] = extracts;
-
-		// Return extracted data with proper parsing
-		return {
-			datetime, level, message,
-			meta: JSON.parse(meta.replace(/([\s]*?)(?=[\{\}\"])/g, ''))
-		};
-
-	}).filter(({ datetime = null }) => {
-
-		// Resolve timestamp from log datetime
-		const dateMoment = moment(datetime);
-		const timestamp = dateMoment.unix();
-
-		// Verify that log falls within the required timestamp range
-		return datetime && dateMoment.isValid() && timestamp >= fromMoment && timestamp <= toMoment;
-
-	});
-
-	// Return the final logs success response
-	return res.json({ status: 'success', logs });
 
 });
 
